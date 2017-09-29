a9dd74f0f011d619d327459547e5cdf5539ebc54
Kieran <kieran8tor@gmail.com>
2017-09-29 13:44:26 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/kieran/coinflip/Flipcoin.java
@@ -0,0 +1,14 @@
+package com.example.kieran.coinflip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Flipcoin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flipcoin);
+    }
+
+}
